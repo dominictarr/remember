ac262a230d0c711fc3690655d5b0a8b449706ef3
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-10 17:42:50 +0700

    test new api

--- test/index.js
+++ test/index.js
@@ -5,49 +5,50 @@ var emitter2 = new Emitter()
 var remember = require('..')
 
 var rmrf = require('rimraf')
-var kv   = require('kv')
+var fs = require('fs')
 var dir = '/tmp/remember-test'
 
-var assert = require('assert')
+require('tape')(function (assert) {
 
-rmrf(dir, function () {
+  rmrf(dir, function () {
   
-  var sync = remember(kv(dir))
+    var sync = remember(fs)
 
-  sync(emitter, 'messages')
+    sync(emitter, '/tmp/remember-test-messages', function () {
+      console.log('synced1')
+    })
 
-  var expected = [], actual = []
+    var expected = [], actual = []
 
-  /*
-    create two scuttlebutt/events instances that will 
-  */
+    /*
+      create two scuttlebutt/events instances that will 
+    */
 
-  emitter.on('message', function (hi) {
-//    console.log('1>>', hi)
-    expected.push(hi)
+    emitter.on('message', function (hi) {
+      console.log('1>>', hi)
+      expected.push(hi)
+    })
+
+    emitter2.on('message', function (hi) {
+      console.log('2>>', hi)
+      actual.push(hi)
+    })
+
+    var i = 0
+    var timer = setInterval(function () {
+      if(i++ < 5)
+        return emitter.emit('message', 'hello_'+new Date())
+      emitter.dispose()
+      clearInterval(timer)
+      sync(emitter2, '/tmp/remember-test-messages', function () {
+        console.log('SYNCED emitter2')
+        //assert they are consistent.
+        console.log('ACTUAL', actual)
+        console.log('EXPECTED', expected)
+        assert.deepEqual(actual, expected)
+        passed = true
+        assert.end()
+      })
+    }, 1e2)
   })
-
-  emitter2.on('message', function (hi) {
-//    console.log('2>>', hi)
-    actual.push(hi)
-  })
-
-  emitter2.on('sync', function () {
-    console.log('SYNCED emitter2')
-    //assert they are consistent.
-    console.log('ACTUAL', actual)
-    console.log('EXPECTED', expected)
-    assert.deepEqual(actual, expected)
-    passed = true
-  })
-
-  var i = 0
-  var timer = setInterval(function () {
-    if(i++ < 5)
-      return emitter.emit('message', 'hello_'+new Date())
-    emitter.dispose()
-    clearInterval(timer)
-    sync(emitter2, 'messages')
-    
-  }, 1e2)
 })
